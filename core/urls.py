d5002ce3ec6a3a770b6c6ac52283f16fde4c87df
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-09 01:52:15 +0300

    Auth finished

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -11,3 +13,7 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
