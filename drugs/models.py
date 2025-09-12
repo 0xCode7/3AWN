016aecc1de164160994c8536db35287ab7fbf826
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-12 06:51:21 +0300

    Add remove sec from time

--- drugs/models.py
+++ drugs/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from authentication.models import User
 
-
-# Create your models here.
 class Medication(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     dosage = models.CharField(max_length=100)
+    # store only hours and minutes, seconds will be ignored
     time = models.TimeField()
     type = models.CharField(max_length=60, default='Tablet')
     times_per_day = models.IntegerField(default=1)
@@ -16,3 +15,9 @@ class Medication(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.user.email})"
+
+    def save(self, *args, **kwargs):
+        # truncate seconds
+        if self.time:
+            self.time = self.time.replace(second=0, microsecond=0)
+        super().save(*args, **kwargs)
