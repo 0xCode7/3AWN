8142e16e4be1b27252d46c934527efe6960abc13
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-12 07:03:04 +0300

    edit mark the dose taken

--- drugs/serializers.py
+++ drugs/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Medication
+from datetime import date
+
 
 class MedicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medication
         fields = '__all__'
         read_only_fields = ['user']
+
+        def create(self, validated_data):
+            today_str = date.today().isoformat()
+            validated_data["dose_taken"] = {
+                today_str: {f"dose-{i + 1}": True for i in range(validated_data.get("times_per_day", 1))}}
+            return super().create(validated_data)
